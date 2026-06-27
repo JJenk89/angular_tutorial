d9fee64c2464f34da8f657f321d67ba09abee031
jjenk <j.jenkinson89@hotmail.com>
2026-06-27 12:19:48 +0200

    Feature - Add housinglocation interface - Add types to housing-location component

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,21 +1,23 @@
 import { Component } from '@angular/core';
 import { HousingLocation } from '../housing-location/housing-location';
+import { HousinglocationInfo } from '../housinglocation';
 @Component({
   selector: 'app-home',
   imports: [HousingLocation],
-  template: ` 
-     <section>
-      <form>
-        <input type="text" placeholder="Filter by city" />
-        <button class="primary" type="button">Search</button>
-      </form>
-    </section>
-
-    <section class="results">
-      <h2>Results</h2>
-      <app-housing-location />
-    </section>
-  `,
+  templateUrl: './home.html',
   styleUrls: ['./home.css'],
 })
-export class Home {}
+export class Home {
+  readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
+
+  housingLocation: HousinglocationInfo = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  }
+}
